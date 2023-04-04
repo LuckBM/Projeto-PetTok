5bd2fdfb72a2ffc5dce744785e7406972d7a1426
LuckBM <116355056+LuckBM@users.noreply.github.com>
2023-04-03 21:18:01 -0300

    Att aula 6/Final

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { collection, getDocs } from 'firebase/firestore/lite'
 
 function App() {
 
+  let maxHeight;
+  if (window.innerHeight <= 800) {
+    maxHeight = window.innerHeight
+  }
+
   const [video, setVideos] = useState([])
 
   async function getVideos() {
@@ -13,16 +18,14 @@ function App() {
     const videosSnapshot = await getDocs(videosCollection)
     const videosList = videosSnapshot.docs.map(doc => doc.data())
     setVideos(videosList)
-
   }
-
 
   useEffect(() => {
     getVideos();
   }, [])
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + "px" }}>
       <div className="app__videos">
         {video.map((item) => {
           return (
@@ -37,8 +40,6 @@ function App() {
             />
           )
         })}
-
-  
       </div>
     </div>
   );
